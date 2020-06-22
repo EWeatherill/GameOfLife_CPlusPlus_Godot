2eebd4a045d8225be7ba6c370eaa0b99a6fb470e
E Weatherill <28298582+EWeatherill@users.noreply.github.com>
2020-06-22 16:24:06 +0100

    Defined some more public methods.  Still requires some design thought.

--- src/GameOfLife.hpp
+++ src/GameOfLife.hpp
@@ -3,43 +3,67 @@
 #include <vector>
 
 /**
-This class is responsible for handling the grid of cells that can
-evolve through generations following Conway's Game of Life rules.
-**/
+ * This class is responsible for handling the grid of cells that can
+ * evolve through generations, following Conway's Game of Life rules.
+ **/
 class GameOfLife{
 
     private:
-        std::pair gridSize;
+        std::pair<int, int> grid_Size;
         std::vector<std::vector<bool>> grid;
-        std::vector<std::vector<bool>> temporaryGrid;
+        std::vector<std::vector<bool>> temporary_Grid;
+        int numberOfEvolutionsSoFar = 0;
 
     public:
 
         /**
-        Create a new Game of Life instance with a grid of cells
-        with the dimensions given.  All cells within the grid are
-        set to false (dead) by default.
-
-        @param gridWith: the number of cells making up the width
-               of the grid
-        @param gridHeight: the number of cells making up the
-               height of the grid
-        **/
-        GameOfLife(int gridWidth, int gridHeight);
+         * Create a new Game of Life instance with a grid of cells
+         * with the dimensions given.  All cells within the grid are
+         * set to false (dead) by default.
+         * 
+         * @param gridWidthInCells: the number of cells making up the width
+         *        of the grid
+         * @param gridHeightInCells: the number of cells making up the
+         *        height of the grid
+         **/
+        GameOfLife(int gridWidthInCells, int gridHeightInCells);
 
         /**
-        TODO:
+         * Set the given list of cells to "alive" (true) state.  The
+         * coordinates are zero-based {column, row}.
+         * 
+         * If any of the coordinates fall outside the range of the grid
+         * they will be ignored.
+         * 
+         * @param cellsCoordinates: a variable list of std::pair<int, int>
+         *        representing the coordinates {column, row} of the cell
+         *        to set as "alive" (true)
+         **/
+        void SetCellsToAlive(std::pair<int, int> const cellsCoordinates...);
 
-        How do I want to handle toggling cell state when the user is selecting
-            the initial cells on the starting grid?
-        Do I want specific methods to "SetCellToDead" and "SetCellToAlive"?
-        It would be good to have methods that can be used during the
-             evolution.
-        Do I want to change cell state one at a time or variable numbers
-             of them at once?
+        /**
+         * Evolve through one generation following Conway's Game of Life
+         * rules.
+         * 
+         * @return: a boolean if grid state changed (i.e. a cell "died" or
+         *          was "born")
+         **/
+        bool Evolve();
 
-        **/
-        void ToggleCellState(std::pair<int, int> cellCoordinates);
+        /**
+         * Get the number of evolutons which have occured so far.
+         * 
+         * @return: an int representing the number of evolutions that
+         *          have occured so far
+         **/
+        int GetCurrentEvolutionNumber() const { return numberOfEvolutionsSoFar; }
 
-
+        /**
+         * Get the current state of the grid.
+         * 
+         * @return: a vector of boolean vectors.  The outermost vector 
+         * represents the x coordinate/column index.  The innermost vector 
+         * represents the y coordinate/row index.
+         **/
+        std::vector<std::vector<bool>> const & const GetGrid() const { return grid; }
 };
